124aa45fee1761bbb949a3671f3d8abe27be3c9d
Jesper Lyager Nielsen <lyager@gmail.com>
2015-01-27 14:12:47 +0100

    Added EasyMotion

--- update_plugins.py
+++ update_plugins.py
@@ -39,6 +39,7 @@ syntastic https://github.com/scrooloose/syntastic
 vim-repeat https://github.com/tpope/vim-repeat
 vim-commentary https://github.com/tpope/vim-commentary
 vim-go https://github.com/fatih/vim-go
+vim-easymotion https://github.com/Lokaltog/vim-easymotion/
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
